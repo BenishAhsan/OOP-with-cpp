64249c51a56ae55dd55bbc25668d610c10acca6b
Binash Ahsan <156318355+BenishAhsan@users.noreply.github.com>
2025-07-29 22:37:02 +0500

    Update structs.cpp

--- Structs/structs.cpp
+++ Structs/structs.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
 int main(){
+// our struct for storing student info
+
+struct Student
+{
+    string name;
+    string student_id;
+    int subjects_num;
+};
+
+int main()
+{
+    // use of the above Student struct
+    Student student1; // declared a variable student1 of type Student
+    student1.name = "Binash Ahsan";
+    student1.student_id = "SE123";
+    student1.subjects_num = 3;
 
     
     return 0;
